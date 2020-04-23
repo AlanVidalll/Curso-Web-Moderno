6ddb5d9553129bac59bb2e952b789fb5753b2f6b
Alan Vidal <alanfonseca7812@gmail.com>
2020-04-23 16:04:43 -0300

    Atualizando arquivos

--- Lista Exercicios/exercicio12.js
+++ Lista Exercicios/exercicio12.js
@@ -1,11 +1,8 @@
 /*12) Faça um algoritmo que calcule o fatorial de um número.*/
 
-
-
-
 function fatorial(valor) {
       
  return valor<2 ? 1 : valor*fatorial(valor-1)
   
 } 
-console.log(fatorial(6))
+console.log(fatorial(6)) 
---
+++ Objeto/oo.js
@@ -0,0 +1,21 @@
+// Código não executável....
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+const objeto = {
+        valor1,
+        valor2,
+        valor3,
+       processamento() {
+        // ....
+    }
+}
+objeto.processamento() // Foco passou ser o objeto e não a funçao...
+
+// Principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 2. Encapsulamento
+// 3. Herança (prototype)
