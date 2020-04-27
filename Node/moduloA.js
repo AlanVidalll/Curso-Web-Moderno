fc58620cdad445d4be7bbeead3261d939f0eacdd
Alan Vidal <alanfonseca7812@gmail.com>
2020-04-27 20:01:35 -0300

    Adicionando arquivos de Node

---
+++ Node/moduloA.js
@@ -0,0 +1,4 @@
+// maniera de exportar em node
+this.ola = 'fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até próximo evento'
---
+++ Node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA')// exportei o moduloA
+const moduloB = require('./moduloB')// exportei o moduloB
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB)
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
